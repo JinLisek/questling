2c1abdfa56e8d340df9681addd2efc51aad7b8a4
jinlisek <tastyramen@gmail.com>
2019-06-16 18:14:20 +0200

    Added new model: Quest - representing task

--- questling/quests/models.py
+++ questling/quests/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Quest(models.Model):
+    header = models.CharField(max_length = 300)
+    description = models.CharField(max_length = 3000, blank = True)
+    author = models.CharField(max_length = 50)
+    created_at = models.DateTimeField(auto_now_add = True)
