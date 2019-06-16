5bcca0ae3784806785598c1af4f17a591b86d6ca
jinlisek <tastyramen@gmail.com>
2019-06-16 17:18:18 +0200

    RESET

--- quests/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from quests.models import Quest
-
-class QuestSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Quest
-        fields = "__all__"
--- quests/urls.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import routers
-from quests.api import QuestViewSet
-
-router = routers.DefaultRouter()
-router.register("api/quests", QuestViewSet, "quests")
-
-urlpatterns = router.urls
